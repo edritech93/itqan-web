8b9f9a808e766208301021cc87e695dded39967c
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-08-01 13:45:24 +0700

    edit screen login

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -1,5 +1,5 @@
 import './css/login.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, TextField, Card } from '@material-ui/core';
 import { STORAGE } from '../../actions/types';
@@ -16,19 +16,34 @@ export default function Login(props) {
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
   const handleLogin = () => {
-    if (username === 'admin' && password === '123qwe')  {
+    if (username === 'admin' && password === '123qwe') {
       localStorage.setItem(STORAGE.TOKEN, '123qwe')
       props.history.push('/home');
     } else {
       alert('salah username atau password')
     }
   }
+
   return (
     <div className="container">
       <Card className="form">
         <h1>Login</h1>
-        <TextField id="outlined-basic" label="Username" variant="outlined" className={classes.form} onChange={(text) => setUsername(text)} />
-        <TextField id="outlined-basic" label="Password" variant="outlined" className={classes.form} onChange={(text) => setPassword(text)} />
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          className={classes.form}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          type="password"
+          className={classes.form}
+          security={true}
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <Button variant={"contained"} color={"primary"} onClick={handleLogin}>
           Login
         </Button>
